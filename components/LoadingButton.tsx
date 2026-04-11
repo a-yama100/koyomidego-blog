d92c92e4265c51f47d72c459e2d96a27a730082a
a-yama100 <yama0226@gmail.com>
2026-04-11 17:16:16 +0900

    クリック可能なbutton要素にcursor-pointerを追加（見落とし分）

--- components/LoadingButton.tsx
+++ components/LoadingButton.tsx
@@ -33,7 +33,7 @@ export function LoadingButton({
   const widthClass = fullWidth ? 'w-full' : ''
   return (
     <button
-      className={baseStyles + ' ' + variants[variant] + ' ' + sizes[size] + ' ' + widthClass + ' ' + className}
+      className={'cursor-pointer disabled:cursor-not-allowed ' + baseStyles + ' ' + variants[variant] + ' ' + sizes[size] + ' ' + widthClass + ' ' + className}
       disabled={disabled || loading}
       {...props}
     >
